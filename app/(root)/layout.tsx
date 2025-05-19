23ad60942b244e24d39dea9ea5af6f387af075bb
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-05-19 13:09:40 +0530

    added auth pages

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { ReactNode } from 'react'
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className='root-layout'>
+            <nav>
+                <Link href="/" className='flex items-center gap-2'>
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className='text-primary-100'>PrepWise</h2>
+                </Link>
+            </nav>
+            {children}
+        </div>
+    )
+}
+
+export default RootLayout
