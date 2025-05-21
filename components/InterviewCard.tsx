dfbc8d293d895376a25a5c8c50f51ed3e7180b67
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-05-21 17:47:59 +0530

    Added Feedback Page Design

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
 import dayjs from 'dayjs'
 import Image from 'next/image'
 import { getRandomInterviewCover } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import DisplayTechIcons from './DisplayTechIcons'
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action'
 
-const InterviewCard = ({ id, userid, role, type, techstack, createdAt }: InterviewCardProps) => {
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
 
-  const feedback = null as Feedback | null
+  const feedback = userId && id
+    ? await getFeedbackByInterviewId({ interviewId: id, userId })
+    : null
   const normalizedType = /mix/gi.test(type) ? 'Mixed' : type
   const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('DD MMM, YYYY')
 
